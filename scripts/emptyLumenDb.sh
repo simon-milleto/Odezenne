6c9c42f658c82abf29b270da4ec2d3bf3eb5dc24
Bram van Osta <bramvanosta@gmail.com>
2017-04-09 12:19:26 +0200

    [Fix] Fixes emptyLumenDb script which broke the migration schema

--- scripts/emptyLumenDb.sh
+++ scripts/emptyLumenDb.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker exec -i o2n_lumen_db sh -c 'exec mysql -u homestead -psecret -Nse "show tables" homestead | while read table; do mysql -u homestead -psecret -e "truncate table $table" homestead; done'
+docker exec -i o2n_lumen_db sh -c 'exec mysql -u homestead -psecret -Nse "show tables" homestead | while read table; do mysql -u homestead -psecret -e "drop table $table" homestead; done'
+docker exec -i o2n_lumen php artisan migrate:install
+docker exec -i o2n_lumen php artisan migrate
