f137f006bbb914ef96d96829f0337d8376fb1e56
Bram van Osta <bramvanosta@gmail.com>
2017-04-09 12:09:47 +0200

    [API] Adds status column to Track table

--- api/lumen/app/Track.php
+++ api/lumen/app/Track.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Track extends Model
 {
-    protected $fillable = ['track_id', 'title', 'artwork_url', 'track_url', 'stream_url', 'total_time'];
+    protected $fillable = ['track_id', 'title', 'artwork_url', 'track_url', 'stream_url', 'total_time', 'status', 'updated_at', 'created_at'];
 }
