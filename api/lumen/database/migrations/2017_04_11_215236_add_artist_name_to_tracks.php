f3b4859bad8e6a951a467205d8b4f0794725f876
Bram van Osta <bramvanosta@gmail.com>
2017-04-12 00:54:03 +0200

    [API] Removes unnecessary total_time attribute + Adds artist name attribute

--- api/lumen/database/migrations/2017_04_11_215236_add_artist_name_to_tracks.php
+++ api/lumen/database/migrations/2017_04_11_215236_add_artist_name_to_tracks.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Track extends Migration
+class AddArtistNameToTracks extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,8 @@ class Track extends Migration
      */
     public function up()
     {
-        Schema::create('tracks', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('track_id');
-            $table->string('title');
-            $table->string('artwork_url')->nullable();
-            $table->string('track_url');
-            $table->string('stream_url');
+        Schema::table('tracks', function (Blueprint $table) {
+            $table->string('artist');
         });
     }
 
