e62b24b66d76725d110fbbf473bd5f796c382890
Bram van Osta <bramvanosta@gmail.com>
2017-04-15 14:57:28 +0200

    [Home] Adds second audioVisualization scene

---
+++ client/src/scenes/CircleScene.vue
@@ -0,0 +1,22 @@
+<template>
+  <canvas ref="canvas"></canvas>
+</template>
+
+<script>
+  import CircleScene from './CircleScene';
+
+  export default {
+    name: 'circle-scene',
+    props: ['analyser'],
+    mounted() {
+      /* eslint-disable no-new */
+      this.CircleScene = new CircleScene(this.$refs.canvas, this.analyser);
+    },
+    beforeDestroy() {
+      this.CircleScene.stopAnimation();
+    },
+  };
+</script>
+
+<style>
+</style>
