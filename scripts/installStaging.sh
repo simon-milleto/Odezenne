148ec2649abaf8203fb316629a7ec5494ff9e148
Bram van Osta <bramvanosta@gmail.com>
2017-06-06 22:48:39 +0200

    [Config] Tweaks on Staging environment

--- scripts/installStaging.sh
+++ scripts/installStaging.sh
@@ -14,3 +14,11 @@ cp "$(pwd)"/api/lumen/.env.staging "$(pwd)"/api/lumen/.env
 # Install Back-end dependencies for Lumen
 echo "### Installing Back-end dependencies for Lumen ###"
 docker run --rm -v "$(pwd)"/api/lumen:/app -w /app composer/composer install
+
+# Setup Lumen database
+docker exec -i o2n_lumen php artisan migrate:install
+docker exec -i o2n_lumen php artisan migrate
+
+# Import Wordpress database
+echo "### Importing Wordpress database ###"
+docker exec -i o2n_wordpress_db mysql -u wordpress -ppassword wordpress < config/wp_dump--staging.sql
