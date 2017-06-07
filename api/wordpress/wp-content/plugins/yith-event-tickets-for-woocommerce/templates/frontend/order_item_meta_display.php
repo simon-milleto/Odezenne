a7ed5c5948ca09c2e9f2df0b10575699d209adbb
Bram van Osta <bramvanosta@gmail.com>
2017-06-07 23:14:54 +0200

    [WP] Removes YithEventTickets plugin & Adds Mailgun plugin

--- api/wordpress/wp-content/plugins/yith-event-tickets-for-woocommerce/templates/frontend/order_item_meta_display.php
+++
@@ -1,9 +0,0 @@
-<?php
-foreach ($meta_items as $item){
-    ?>
-        <dl class="variation">
-            <dt class="variation-product_type"><?php echo $item['label'] ?> :</dt>
-            <dd class="variation-product_type"><?php echo $item['value'] ?></dd>
-        </dl>
-    <?php
-}
