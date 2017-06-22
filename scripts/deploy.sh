a974832decccdb1eab98604f3d2fbe638019d886
Bramvanosta <bramvanosta@gmail.com>
2017-06-22 11:44:58 +0200

    [CI] Changes installStaging path for deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 # Run the installation script
-ssh -o "StrictHostKeyChecking=no" $o2n_user@$o2n_host ls
-ssh -o "StrictHostKeyChecking=no" $o2n_user@$o2n_host .odezenne/scripts/installStaging.sh
+ssh -o "StrictHostKeyChecking=no" $o2n_user@$o2n_host ./odezenne/scripts/installStaging.sh
