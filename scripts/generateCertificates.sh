b94a7a2720c9ac75d3934722d86c78ac0735500a
Bram van Osta <bramvanosta@gmail.com>
2017-06-26 13:32:06 +0200

    [Config] Fixes image versions of the different docker containers

--- scripts/generateCertificates.sh
+++ scripts/generateCertificates.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker run --rm -v "$(pwd)"/config/certs:/app -it nginx openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout /app/privateKey.key -out /app/certificate.crt
+docker run --rm -v "$(pwd)"/config/certs:/app -it nginx:1.11 openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout /app/privateKey.key -out /app/certificate.crt
 
-docker run --rm -v "$(pwd)"/config/certs:/app -it nginx openssl dhparam -out /app/dhparam.pem 2048
+docker run --rm -v "$(pwd)"/config/certs:/app -it nginx:1.11 openssl dhparam -out /app/dhparam.pem 2048
