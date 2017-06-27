dee256a8618415214c4b0b1b4b0f06729d382a57
Guillaume Darricau <g.darricau@hotmail.com>
2017-06-27 17:05:32 +0200

    Wordpress Plugin Guests Generator

---
+++ api/wordpress/wp-content/plugins/o2n_guests/o2n_guests_api.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guillaume
+ * Date: 27/06/2017
+ * Time: 11:28
+ */
+Class O2nGuestsApi
+{
+    /**
+     * Curl options
+     * @var array
+     */
+    private $_curlOptions;
+
+    /**
+     * Soundcloud API url
+     * @var string
+     */
+    private $_apiEndpoint;
+
+    /**
+     * Class constructor
+     * @access public
+     */
+    function __construct()
+    {
+        $this->_setDefaultCurlOptions();
+        $this->_apiEndpoint = getenv('API_HOST') . '/api/v1/';
+    }
+
+    protected function _setDefaultCurlOptions()
+    {
+        $ENV = getenv('ENV');
+        switch ($ENV) {
+            case 'development':
+                $HTTPHEADER = array('Host: lumen.o2n');
+                break;
+            default:
+                $HTTPHEADER = array();
+        }
+
+        $this->_curlOptions = array(
+            CURLOPT_HEADER => false,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_USERAGENT => 'O2N Wordpress',
+            CURLOPT_HTTPHEADER => $HTTPHEADER,
+        );
+    }
+
+    protected function _request($path, $curlOptions = array())
+    {
+        try {
+            $options = $this->_curlOptions;
+            $options += $curlOptions;
+
+            $curl = curl_init($path);
+            curl_setopt_array($curl, $options);
+            $response = curl_exec($curl);
+
+            if (FALSE === $response)
+                throw new Exception(curl_error($curl), curl_errno($curl));
+
+            curl_close($curl);
+
+            return $response;
+
+        } catch (Exception $error) {
+            trigger_error(sprintf(
+                'API request failed with error #%d: %s',
+                $error->getCode(), $error->getMessage()),
+                E_USER_ERROR);
+
+        }
+    }
+
+    function setSettings($settings)
+    {
+        $formattedUrl = $this->_apiEndpoint . 'settings';
+
+        $params = array(
+            CURLOPT_POST => 1,
+            CURLOPT_POSTFIELDS => $settings
+        );
+
+        $data = $this->_request($formattedUrl, $params);
+
+        return $data;
+    }
+}
