7a2bab74f2c649e27994e2159511c59e454be5c3
Bram van Osta <bramvanosta@gmail.com>
2017-06-30 10:42:50 +0200

    [Vue] Fixes UI of facebook post

--- client/src/components/Social/Facebook.vue
+++ client/src/components/Social/Facebook.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="">
-    <iframe scrolling="no" frameborder="0" allowTransparency="true" width="500" height="523" style="border:none;overflow:hidden"
-            :src="'https://www.facebook.com/plugins/post.php?href=' + facebook.permalink_url" ></iframe>
+    <div class="fb-post" :data-href="facebook.permalink_url" data-width="500" data-show-text="true">
+      <blockquote :cite="facebook.permalink_url" class="fb-xfbml-parse-ignore">
+        Publié par <a href="https://www.facebook.com/facebook/">Facebook</a> sur&nbsp;
+        <a :href="facebook.permalink_url">jeudi 27 août 2015</a>
+      </blockquote>
+    </div>
   </div>
 </template>
 
@@ -10,6 +14,14 @@
     name: 'facebook',
     props: ['facebook', 'position'],
     mounted() {
+      /* eslint-disable */
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.9";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
     },
   };
 </script>
